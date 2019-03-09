aedae19ba7586ce769511157d3b51db3d8446fdc
阿笨哥 <abanger@qq.com>
2019-03-09 10:09:08 +0800

    create:data_proc001

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # DCIC2019-Concrete-Pump-Vehicles
-混凝土泵车砼活塞故障分析
+混凝土泵车砼活塞故障预警分析
+
+## 前言
+初学者，由于工作时间比较紧，准备上传个人一些想法，欢迎指正交流。
+
+- data_proc001_type.py，在参考[3]取得0.619
+- ...
+
+
+## 主要参考
+1. [DCIC-Failure-Prediction-of-Concrete-Piston-for-Concrete-Pump-Vehicles](https://github.com/jmxhhyx/DCIC-Failure-Prediction-of-Concrete-Piston-for-Concrete-Pump-Vehicles)
+2. [混凝土泵车砼活塞故障预警](https://github.com/tianshuaifei/dcic_2019)
+3. [Data Fountain光伏发电量预测 Top1 开源分享](https://zhuanlan.zhihu.com/p/44755488?utm_source=qq&utm_medium=social&utm_oi=623925402599559168)
+
+## 讨论
+[留言](https://github.com/abanger/DCIC2019-Concrete-Pump-Vehicles/issues)
---
+++ data_proc001_type.py
@@ -0,0 +1,52 @@
+# #  数据整合，未处理
+
+import pandas as pd
+import os
+from tqdm import *
+path="./"
+data_list = os.listdir(path+'data_train/')
+
+
+def add_device_type(devicetype,lenbb):
+    chadd = pd.DataFrame(columns = ['ZVe44', 'ZV573',  'ZV63d', 'ZVfd4',  'ZVa9c',  'ZVa78',  'ZV252'])
+    for i in range(lenbb): #插入一行
+        chadd.loc[i] = [0  for n in range(7)]    
+    chadd[devicetype]=1
+    #print(devicetype)
+    return chadd
+
+
+file_name='data/data_all_n2_type.csv'
+df = pd.read_csv(path+'data_train/'+ data_list[0])
+lenbb=len(df)
+devicetype=df.loc[0]['设备类型']
+df = pd.concat( (df,add_device_type(devicetype,lenbb) ),axis=1)
+df['sample_file_name'] = data_list[0]
+
+df.to_csv(file_name, index=False,encoding='utf-8')
+
+for i in tqdm(range(1, len(data_list))):
+    if data_list[i].split('.')[-1] == 'csv':
+        df = pd.read_csv(path+'data_train/' + data_list[i])
+        lenbb=len(df)
+        devicetype=df.loc[0]['设备类型']
+        df = pd.concat( (df,add_device_type(devicetype,lenbb) ),axis=1)
+        df['sample_file_name'] = data_list[i]
+        df.to_csv(file_name, index=False, header=False, mode='a+',encoding='utf-8')
+    else:
+        continue
+
+
+test_data_list = os.listdir(path+'data_test/')
+
+
+for i in tqdm(range(len(test_data_list))):
+    if test_data_list[i].split('.')[-1] == 'csv':
+        df = pd.read_csv(path+'data_test/' + test_data_list[i])
+        lenbb=len(df)
+        devicetype=df.loc[0]['设备类型']
+        df = pd.concat( (df,add_device_type(devicetype,lenbb) ),axis=1)        
+        df['sample_file_name'] = test_data_list[i]
+        df.to_csv(file_name, index=False, header=False, mode='a+',encoding='utf-8')
+    else:
+        continue
