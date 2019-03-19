a0afa0b1e09e4baf33ea1f8b3e0f011b1482b083
阿笨哥 <abanger@qq.com>
2019-03-19 14:05:35 +0800

    updat:cnn100.py

---
+++ cnn100.py
@@ -0,0 +1,106 @@
+## 处理data_train2cnn100.py 生成数据，过程
+import os
+import sys
+from keras import backend as K
+from keras.models import Sequential,load_model
+from keras.layers import Dense,LSTM,Dropout
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+import numpy as np
+#coding:utf-8
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = ""    ###这两句代码用来关闭CUDA加速
+from keras.models import Input,Model
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPool2D,GlobalAveragePooling2D
+from keras.layers import BatchNormalization,Dense
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+import glob
+import random
+import numpy as np
+img_h,img_w,n_channels = 20,20,1 ####图像高，宽，通道数
+classes = 2 ###类别
+train_batch_size = 40  
+test_batch_size= 20   
+
+import pickle
+import gc
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+#import lightgbm as lgb
+from sklearn.model_selection import StratifiedKFold
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+np.random.seed(2018)
+print("load_data")
+
+
+train_data=np.load('data_train_cnn100.npy')
+train_lable=np.load('label_train_cnn100.npy')
+
+##CNN输入的数据预处理
+'''
+lent=train_data.shape[0]
+ddtrain_data =np.empty((lent,1,20,20),dtype="float32")
+for i in range(0,lent):
+    ddtrain_data[i,0,:,:]  = train_data[i,0,:,: ]-np.mean(train_data[i,0,:,: ], axis = 0)
+    if i%8000==0:
+        print(str(i))
+'''
+
+x_train,x_val,y_train,y_val = train_test_split(train_data,train_lable)
+#x_train,x_val,y_train,y_val = train_test_split(ddtrain_data,train_lable)
+x_train = x_train.astype('float32')
+x_val = x_val.astype('float32')
+train_data = x_train /255
+test_data=x_val/255
+
+
+# dimensions of our images.
+img_width, img_height = 20, 20
+K.set_image_dim_ordering('th') 
+
+input_data = Input(shape=(n_channels,img_h,img_w))
+out = Conv2D(filters=64,kernel_size=(3,3),strides=(1,1),padding='same',activation='relu')(input_data)
+out = Conv2D(filters=64,kernel_size=(3,3),strides=(1,1),padding='same',activation='relu')(out)
+out = BatchNormalization(axis=3)(out)
+'''
+out = MaxPool2D(pool_size=(2,2),strides=(2,2))(out)
+out = Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu')(out)
+out = Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu')(out)
+out = BatchNormalization(axis=3)(out)
+out = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(out)
+out = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu')(out)
+out = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu')(out)
+out = BatchNormalization(axis=3)(out)
+out = GlobalAveragePooling2D()(out)
+'''
+####out = Dense(units=512,activation='relu')(out) ###数据集比较小，可不加这一层
+##out = Dense(units=5,activation='softmax')(out)
+#top_model.add(Dense(256, activation='relu'))
+#top_model.add(Dropout(0.5))
+#out = Dense(units=64,activation='relu')(out)
+#out = Dropout(0.5)(out)
+#out = Flatten()(out)
+out = GlobalAveragePooling2D()(out)
+out = Dense(units=1,activation='sigmoid')(out)
+model = Model(inputs=input_data,outputs=out)
+model.summary()
+
+model.compile(optimizer=Adam(lr=0.001),loss='binary_crossentropy',metrics=['accuracy'])
+#model.compile(loss='binary_crossentropy', optimizer=SGD(lr=1e-4, momentum=0.9), metrics=['accuracy'])
+#model.compile(optimizer='rmsprop', loss='binary_crossentropy',  metrics=['accuracy'])
+history=model.fit(x_train, y_train, batch_size=128, epochs=200,  verbose=1, validation_data=(x_val, y_val))
+model.save('model_cnn119-129.h5')
+
+score1 = model.evaluate(x_val, y_val, verbose=0)
+print(score1)
+y_val_pred=model.predict(x_val)
+rmse=mean_squared_error(y_val,y_val_pred)**0.5
+score=1.0/(1.0+rmse)
+print('score:',score)
+score_all=f1_score(y_val,y_val_pred>0.5 )
+print('f1_score:',score_all)
---
+++ data_train2cnn100.py
@@ -0,0 +1,115 @@
+#类似图像处理情况（处理数据）
+##https://www.colabug.com/4895309.html
+import os
+#from PIL import Image
+import numpy as np
+import pandas as pd
+#from tqdm import *
+#活塞工作时长,发动机转速,油泵转速,泵送压力,液压油温,流量档位,分配压力,排量电流,低压开关,高压开关,搅拌超压信号,正泵,反泵,设备类型
+def add_device_type(devicetype,lenbb):
+    chadd = pd.DataFrame(columns = ['ZVe44', 'ZV573',  'ZV63d', 'ZVfd4',  'ZVa9c',  'ZVa78',  'ZV252'])
+    for i in range(lenbb): #插入一行
+        chadd.loc[i] = [0  for n in range(7)]    
+    chadd[devicetype]=1
+    #print(devicetype)
+    return chadd
+
+
+path="./"
+    
+##训练数据
+#直接每个文件平均数据
+#path=r"e:\notebook\2019CCF-cd3\data_train"
+fm=os.listdir(path+'data_train/') 
+
+
+'''
+doclen=0
+for f in fm: 
+    #ff= os.listdir(path+'\\'+f+'\\') 
+    td=pd.read_csv(path+'/data_train/'+f )
+    tlen=td.shape[0]
+    if tlen%20!=0:
+        tlen=tlen+20-tlen%20
+    doclen+=tlen
+print(doclen)
+'''
+doclen=10460160//20
+i=0
+chunks = []
+train_data=[]
+train_lable=pd.read_csv(path+"data/train_labels.csv")
+
+data = np.empty((doclen,1,20,20),dtype="float32")
+label = np.empty((doclen,))
+
+#for f in tqdm(fm):  
+for f in fm: 
+    #ff= os.listdir(path+'\\'+f+'\\')  
+    td=pd.read_csv(path+'/data_train/'+f )
+    #删除异常数据
+    td.drop(td[td['活塞工作时长']>150].index)  ##50
+    td.drop(td[ (td['发动机转速']>10000) ].index)
+    td.drop(td[  (td['油泵转速']>15000) ].index)
+    #td.drop(td[ td['泵送压力']>450 ].index)  ##>0
+    td.drop(td[ td['排量电流']>5000 ].index)  ##由训练数据图表观察到异常      
+
+    td["活塞工作时长"]=np.log1p(td["活塞工作时长"]) *25.5
+    td["发动机转速"]=np.log1p(td["发动机转速"]) *25.5
+    td["油泵转速"]=  np.log1p(td["油泵转速"]) *25.5
+    #td["pump_pressure"]=np.log1p(td["pump_pressure"])
+    td["液压油温"]=np.log1p(td["液压油温"]+70) *25.5
+    #td["pressure"]=np.log1p(td["pressure"])
+    td["排量电流"]=np.log1p(td["排量电流"]) *25.5
+
+
+    devicetype=td.loc[0]['设备类型']
+    ff=train_lable[train_lable['sample_file_name']==f ]['label']
+    #td.insert(0,'label',ff.values[0])    
+    #td.insert(0,'sample_file_name',f)
+    bb=td.drop(['设备类型'], axis=1)
+    lenbb=bb.shape[0]
+    a=bb.mean(axis=0)  
+    aa=pd.DataFrame(a)
+    aa=aa.T
+    #aa.insert(0,'sample_file_name',f)
+    bb=pd.concat( (bb,add_device_type(devicetype,lenbb) ),axis=1)
+    aa=pd.concat( (aa,add_device_type(devicetype,1) ),axis=1)
+    if lenbb<20:
+        for ii in range(lenbb,20):
+            bb=bb.append(aa,ignore_index=True)
+        data[i,:,:,:]  =  bb.values
+        label[i] =ff.values[0]
+        i+=1
+    else:
+        #cc=bb
+        a=lenbb//20
+        j=0
+        for j in range(1,a+1): 
+            dd=bb[(j-1)*20:j*20]
+            data[i,:,:,:]  =  dd.values
+            label[i] =ff.values[0]
+            i+=1
+        cc=bb[j*20:lenbb]
+        if (lenbb%20!=0):
+            for k in  range(lenbb,lenbb+20-lenbb%20):
+                cc=cc.append(aa,ignore_index=True)
+            data[i,:,:,:]  =  cc.values
+            label[i] =ff.values[0]
+            i+=1    
+        #bb=cc
+    #train_data.append(bb)
+
+    if i%500==0:
+        print('read:'+str(i))
+    #    break;
+    #data[i,:,:,:]  =  bb.values
+    #label[i] =ff.values[0]
+
+        
+#train_data = pd.concat(train_data, ignore_index=True)
+
+#train_data.to_csv('train_data_2cnn100.csv',index=False,encoding='utf-8')
+np.save('data_train_cnn100.npy', data)
+np.save('label_train_cnn100.npy', label)
+
